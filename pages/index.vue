fc4d236243e2a33dcbafb747bd66de64a4a837a0
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2020-10-09 15:43:36 +0300

    added initial project structure

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home-page">
+    <p>Some content here!</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ plugins/global-components.js
@@ -0,0 +1,6 @@
+// import Vue from 'vue'
+// import Component from '@/components/common/Component'
+
+// Vue.use(someModule, { settings })
+
+// Vue.component('Component', Component)
