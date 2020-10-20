09a1bc512db98430425396c4e15e2e93d72570fc
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2020-10-20 10:28:14 +0300

    updated styles structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['~assets/scss/core/index.scss'],
+  css: ['~node_modules/normalize.css', '~assets/scss/core/index.scss'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
@@ -53,7 +53,8 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
   ],
   /*
    ** Nuxt.js modules
@@ -72,6 +73,48 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {},
+  vuetify: {
+    customVariables: ['~assets/scss/core/variables.scss'],
+    silent: false,
+    treeShake: true,
+    breakpoint: {
+      scrollBarWidth: 16,
+      thresholds: {
+        xs: 600,
+        sm: 960,
+        md: 1280,
+        lg: 1920
+      }
+    },
+    icons: {
+      iconfont: 'mdi',
+      values: {}
+    },
+    rtl: false,
+    theme: {
+      default: 'light',
+      dark: false,
+      disable: false,
+      options: {
+        cspNonce: undefined,
+        customProperties: true,
+        minifyTheme: undefined,
+        themeCache: undefined
+      },
+      themes: {
+        light: {
+          primary: '#8c30f5',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107'
+        },
+        dark: {}
+      }
+    }
+  },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="home-page">
-    <p>Some content here!</p>
+    <v-main>
+      <v-row>
+        <v-col>
+          <v-row>
+            <v-col><p class="main-text">Welcome to</p></v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              ><img
+                src="~assets/img/logo.png"
+                class="landify-logo"
+                alt="Landify logo"
+            /></v-col>
+          </v-row>
+          <v-row>
+            <v-col><p class="main-text right">setup wizard!</p></v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-btn nuxt large color="purple900">Start setup now</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-main>
   </div>
 </template>
 
